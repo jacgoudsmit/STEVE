0c122cf0def1d0f781dd0d2411e0a37bfda9d0f5
Jac Goudsmit <jac.goudsmit@gmail.com>
2023-07-27 13:01:01 -0700

    Created sample project for CFA480128Ex-039Tx display (also known as CFA10099)

---
+++ Examples/CFA480128Ex-039Tx/CFA480128Ex-039Tx.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		CFA480128Ex_039Tx.ino
+ Created:	2023-07-27 12:53:49
+ Author:	Jac Goudsmit
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
